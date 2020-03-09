d9b9ddaab514aef20d5363f4d1ea74d7fec4a1f9
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-09 14:24:09 +0100

    changed seed Item images

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ require "faker"
 
 Item.destroy_all
 
-images_array = ["https://pixabay.com/fr/photos/cat-des-animaux-animal-de-compagnie-3449999/", "https://pixabay.com/fr/photos/cat-animaux-des-animaux-grey-cat-3261420/", "https://pixabay.com/fr/photos/cat-portrait-des-animaux-animaux-3374422/", "https://pixabay.com/fr/photos/cat-cat-s-eyes-%C5%93il-des-animaux-2143332/", "https://pixabay.com/fr/photos/gueule-de-bois-cat-visage-de-chat-4864605/", "https://pixabay.com/fr/photos/des-animaux-cat-chaton-b%C3%A2illements-339400/", "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720__340.jpg", "https://pixabay.com/fr/photos/cat-animal-de-compagnie-yeux-468232/", "https://pixabay.com/fr/photos/cat-yeux-bleus-animal-de-compagnie-3336579/"]
+images_array = ["https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg", "https://cdn.pixabay.com/photo/2013/01/25/13/03/cat-76116_960_720.jpg", "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_960_720.jpg", "https://cdn.pixabay.com/photo/2018/01/28/12/37/cat-3113513_960_720.jpg", "https://cdn.pixabay.com/photo/2017/03/29/09/59/cat-2184682_960_720.jpg", "https://cdn.pixabay.com/photo/2013/04/01/03/45/cat-98359_960_720.jpg", "https://cdn.pixabay.com/photo/2013/01/25/13/03/cat-76116_960_720.jpg", "https://cdn.pixabay.com/photo/2015/02/25/17/56/cat-649164_960_720.jpg", "https://cdn.pixabay.com/photo/2017/03/14/14/49/cat-2143332_960_720.jpg"]
 9.times do |i|
   item = Item.create!(title: Faker::Creature::Cat.name, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: Faker::Number.decimal(l_digits: 2), image_url: images_array[i-1])
 end
