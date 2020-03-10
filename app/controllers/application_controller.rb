b984110ceafacde461afb861546c266d2809dc17
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-10 12:49:22 +0100

    cart is created when user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def create_cart(user)
+    if !user.cart
+      user_cart = Cart.create(user: user)
+      flash[:notice] = "Cart created"
+      puts "#"*200
+      puts "cart created"
+    end
+  end
 end
---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,15 @@
+class CartsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update]
+  def create
+    @user = current_user
+    user_cart = Cart.create(user: @user)
+    flash[:notice] = "Cart created"
+  end
+
+  def update
+    @user = current_user
+
+  end
+
+  
+end
