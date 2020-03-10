c34406f3b0682c58e045bc55e7775e9355662bc6
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-10 11:01:03 +0100

    finalized login/signup functions and set navbar responsive when user is connected or not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+ 
+    def flash_class(level)
+      case level
+          when 'notice'
+            "alert alert-primary"
+          when 'success'
+            "alert alert-success"
+          when 'error'
+            "alert alert-danger"
+          when 'alert'
+            "alert alert-danger"
+      end
+  end
+ 
+  
 end
