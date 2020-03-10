c95b67cbac8db708353fc1c72470981c91c6b465
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-10 10:10:59 +0100

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_one :cart
+    has_one :cart, dependent: :destroy
     
 end
