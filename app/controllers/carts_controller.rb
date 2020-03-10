7b32eda1b1817ac655c3dd8cf0820c33543932bd
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-10 19:59:11 +0100

    fixed button pointing to user.id instead of user.cart.id and made impossible to visit someone else's cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update]
+  before_action :authenticate_user!, only: [:create, :update, :show]
   def create
     @user = current_user
     user_cart = Cart.create(user: @user)
@@ -7,6 +7,7 @@ class CartsController < ApplicationController
   end
 
   def update
+    isCurrentUserAuthorizedToBeHere(params[:id])
     @user = current_user
     @item = Item.find(params[:itemId])
 
@@ -15,8 +16,18 @@ class CartsController < ApplicationController
   end
 
   def show
+    isCurrentUserAuthorizedToBeHere(params[:id])
     @useritems = current_user.cart.items
     @price = 0
+  end
+
+  private
+
+  def isCurrentUserAuthorizedToBeHere(id)
+    if current_user.cart.id != id.to_i
+      flash[:alert] = "Vous ne pouvez pas accéder à ce panier car ce n'est pas le vôtre! petit malin ;) !"
+      redirect_to root_path
+    end
   end
 
 end
