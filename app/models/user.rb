d2968204df7de4736c586661c596f86e3baf41ab
Charlie Carpene <solunacisv@gmail.com>
2020-03-10 13:13:02 +0100

    changes from pulling new dev branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # after_create :do_something
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
     has_one :cart, dependent: :destroy
     
+
+    # def do_something
+    #   puts "#"*60
+    #   puts "Done something"
+    #   user_cart = Cart.create(user: current_user)
+    #   flash[:notice] = "Cart created"
+    # end
 end
