b226939862ec4c4ef581773c8d6b808aba41616d
Charlie Carpene <solunacisv@gmail.com>
2020-03-11 18:51:19 +0100

    only dev not prod mailer for recap order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,27 +16,34 @@ class OrdersController < ApplicationController
       email: current_user.email,
       source: params[:stripeToken],
     })
+
     begin
+
     charge = Stripe::Charge.create({
       customer: customer.id,
       amount: @amount,
       description: 'Rails Stripe customer',
       currency: 'eur',
     })
+
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to cart_path(cart.id)
     end
-        flash[:success] = "Votre paiement a fonctionné. Vous allez recevoir sur #{current_user.email} les photos commandées. Merci pour votre achat!"
-        items_ids_to_add = cart.items.to_a.map {|item| item.id }
-        order = Order.new(user: current_user )
-        items_ids_to_add.each do |item_id|
-          order.items.push(Item.find(item_id))
-        end
-        order.save
-        current_user.cart.clearCart
-        # use mailer to send photos
-        redirect_to root_path
+
+    flash[:success] = "Votre paiement a fonctionné. Vous allez recevoir sur #{current_user.email} les photos commandées. Merci pour votre achat!"
+    items_ids_to_add = cart.items.to_a.map {|item| item.id }
+    order = Order.new(user: current_user)
+
+    items_ids_to_add.each do |item_id|
+      order.items.push(Item.find(item_id))
+    end
+
+    order.save
+    UserMailer.user_order(order).deliver_now
+    current_user.cart.clearCart
+    # use mailer to send photos
+    redirect_to root_path
   end
 
 end
