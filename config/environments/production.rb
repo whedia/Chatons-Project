32904e04d24a82780323603d4af83a779aa2024b
Charlie Carpene <solunacisv@gmail.com>
2020-03-11 14:39:37 +0100

    debug mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'https://test-chaton.herokuapp.com/' }
+  config.action_mailer.delivery_method = :mailjet
+
 end
