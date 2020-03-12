47e5343fb7770e698566999ae2550f38820c906a
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-12 15:32:28 +0100

    votre_profil_functional

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
   has_many :items, through: :order_items
+
+  def returnTotalPrice
+    total_price = 0
+    self.items.each do |item|
+       total_price += item.price
+    end 
+    return total_price
+  end
 end
