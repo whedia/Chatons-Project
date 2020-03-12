b5f1013f2061ea35dea0c0cffa4c7bc83bfdb8ed
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-12 17:39:41 +0100

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,8 @@ puts "  Done"
 puts "Destroy all orders..."
 Order.destroy_all
 puts "  Done"
+OrderItem.destroy_all
+CartItem.destroy_all
 
 puts "Create 9 items..."
 images_array = ["https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_960_720.jpg", "https://cdn.pixabay.com/photo/2013/01/25/13/03/cat-76116_960_720.jpg", "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_960_720.jpg", "https://cdn.pixabay.com/photo/2018/01/28/12/37/cat-3113513_960_720.jpg", "https://cdn.pixabay.com/photo/2017/03/29/09/59/cat-2184682_960_720.jpg", "https://cdn.pixabay.com/photo/2013/04/01/03/45/cat-98359_960_720.jpg", "https://cdn.pixabay.com/photo/2013/01/25/13/03/cat-76116_960_720.jpg", "https://cdn.pixabay.com/photo/2015/02/25/17/56/cat-649164_960_720.jpg", "https://cdn.pixabay.com/photo/2017/03/14/14/49/cat-2143332_960_720.jpg"]
@@ -28,8 +30,12 @@ puts "  Done"
 puts "Create test user..."
 test_user = User.create!(email: 'test@example.com', password: "password", password_confirmation: "password")
 puts "  Done. Use 'test@example.com' as login and 'password' as password"
-puts "Create test cart associated to test_user"
-test_cart = Cart.create!(user: User.last)
+puts "Create test admin user..."
+admin_user = User.create!(email: 'super_test_admin@amazing.com', password: "thatstrulyincredible", password_confirmation: "thatstrulyincredible", admin: true)
+admin_cart = Cart.create!(user: admin_user)
+puts "  Done"
+puts "Create test cart associated to test_user..."
+test_cart = Cart.create!(user: test_user)
 puts "  Done"
 puts "Push one item in test_user's cart."
 test_cart.items.push(Item.all.last)
